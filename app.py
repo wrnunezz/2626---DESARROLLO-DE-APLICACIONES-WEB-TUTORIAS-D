001ea9cf355ced18d99fe084c78f2e3cf602adc2
wrnunezz <wr.nunezz@uea.edu.ec>
2026-08-26 19:00:20 -0500

    Actulización clase Tutoría 26_08_2026

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 # importar flask 
 from flask import Flask,render_template,request,redirect,url_for
 
+# Importar formulario de productos Form 
+
+from forms.producto_form import ProductoForm
 # crear una instancia de la aplicación Flask
 app = Flask(__name__)
 
+# Clave secreta para el formulario Flkask wtf 
+app.config['SECRET_KEY'] = 'clave_secreta-ferreteria'
+
+# Lista temporal de productos 
+lista_productos = [
+        {'id': 1, 'nombre': 'Martillo', 'precio': 10.99, 'stock': 50},
+        {'id': 2, 'nombre': 'Taladro', 'precio': 19.99, 'stock': 0},
+        {'id': 3, 'nombre': 'Destornillador', 'precio': 5.99, 'stock': 100}
+    ]
 # definir las rutas de la aplicación
 @app.route('/')
 def home():
@@ -14,15 +26,33 @@ def home():
 def productos():
 
 
-    lista_productos = [
-        {'id': 1, 'nombre': 'Martillo', 'precio': 10.99, 'stock': 50},
-        {'id': 2, 'nombre': 'Taladro', 'precio': 19.99, 'stock': 0},
-        {'id': 3, 'nombre': 'Destornillador', 'precio': 5.99, 'stock': 100}
-    ]
-    return render_template('productos.html', productos=lista_productos)
+    return render_template(
+        'productos.html',
+        productos=lista_productos
+    )
+
+
+@app.route('/productos/nuevo', methods=['GET', 'POST'])
+def nuevo_producto():
+
+    form = ProductoForm()
+
+    if form.validate_on_submit():
+        nuevo ={ 'id':len(lista_productos) +1, 'nombre':form.nombre.data,'precio': form.precio.data,'stock': 0 }
+        lista_productos.append(nuevo)
+
+
+        return redirect(url_for('productos'))
+
+    return render_template(
+        'formulario_producto.html',
+        form=form
+    )
 # RUTA DE EDITAR PRODUCTOS
 @app.route('/productos/editar/<int:id>')
 def editar_producto(id):
+
+    
 
     # Aquí puedes implementar la lógica para editar un producto según su ID
     return f'Editar producto con ID: {id}'
@@ -38,13 +68,12 @@ def eliminar_producto(id):
 # ruta de clientes
 @app.route('/clientes')
 def clientes():
-
+# listado de clientes 
     listado_clientes = [
         {'id': 1, 'nombre': 'Juan Pérez', 'email': 'juap@gmail.com' },
         {'id':2, 'nombre': 'Pedro','email':'predro@gmail.com'}
         ]
          
-    
     return render_template('clientes.html', clientes=listado_clientes)
 
 # ruta de editar clientes 
@@ -60,6 +89,9 @@ def eliminar_cliente(id):
     return f'Eli el cliende id :{id}'
 # ruta de proveedores
 @app.route('/proveedores')
+# listado de proveedores 
+
+
 def proveedores():
     return render_template('proveedores.html')
 
--- forms/producto_form.py
+++ forms/producto_form.py
@@ -1,11 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,DecimalField,IntegerField,SubmitField
-from wtforms.validators import DataRequired, length, email
+from wtforms import StringField, DecimalField, SubmitField
+from wtforms.validators import DataRequired, Length
 
-# clase producto
 
-class Producto(FlaskForm):
+class ProductoForm(FlaskForm):
 
-    nombre=StringField('Nombre', validators=[DataRequired(),length(min=2,max=200,message='El nombre debe tener al menos 2 caracteres y maximo 200')])
-    precio=DecimalField('Precio',validator=[DataRequired()])
-    
+    nombre = StringField(
+        'Nombre',
+        validators=[
+            DataRequired(),
+            Length(min=2, max=200, message='El nombre debe tener al menos 2 caracteres y máximo 200' )
+        ]
+    )
+
+    precio = DecimalField(
+        'Precio',
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    submit = SubmitField('Guardar')
